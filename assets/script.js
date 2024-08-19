b901f2b6294e9d3ad49702566b48a1ca64d4c9ed
Knock <enoch10evelyn@gmail.com>
2024-08-19 03:18:37 -0400

    Created attack and defeat messages

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ var playerAttack = 20;
 
 var foeName = "Zero";
 var foeHealth = 60;
-var foeAttack = 10;
+var foeAttack = 20;
 
 //Function expression 
 var fight = function(){
@@ -19,10 +19,30 @@ var fight = function(){
  foeHealth = foeHealth - playerAttack;
 console.log(  playerName + " attacked " + foeName + "! " + foeName + " has " + foeHealth + " health left");
 
+if( foeHealth === 0) {
+
+    console.log(foeName + " was devastated!!!")
+}
+else{
+  console.log(foeName + " is not done yet!!!")
+}
+
+
 //Foe Attack Turn
   playerHealth = playerHealth - foeAttack;
   console.log( foeName + " attacked " + playerName + "! " + playerName + " has " + playerHealth + " health left");
 
+
+
+    
+  if( playerHealth === 0) {
+    console.log(playerName + " was knocked out ")
+  }
+else {
+    console.log (playerName + " is still in the fight.");
+
 }
+
+};
 
 fight();

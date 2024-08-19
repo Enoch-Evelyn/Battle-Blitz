b9c76b846003970f28e8e08382c82cd9fc3b5b6e
Knock <enoch10evelyn@gmail.com>
2024-08-19 00:35:52 -0400

    Fight Function Created

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//This is the initial fight function that greets/informs the user and registers the user's name 
+function fight(){
+alert(" Welcome to Battle Blitz! This is a world tournament where the combatants have to rush and break through multiple opponents one after another thus a Blitz!!")
+const playerName = prompt("Let's register you for combat.  What is your name ? ")
+console.log(playerName);
+}
+
+fight();
